ef2c4fed24050d3029e7a71eb059fa868487e6e9
Denis <denis03122002@mail.ru>
2023-11-14 16:24:05 +0700

    Страница курса добавлена

--- app/main/routes.py
+++ app/main/routes.py
@@ -56,54 +56,10 @@ def courses():
     }
     indexed_filter_dict = enumerate(filter_dict.items())
 
-    courses_data = [
-    {
-        'id': 1,
-        'title': 'Введение в Python',
-        'description': 'Освойте основы программирования на Python и начните свой путь в мир разработки.',
-        'technology': 'Python'
-    },
-    {
-        'id': 2,
-        'title': 'Продвинутое программирование на Java',
-        'description': 'Разберитесь с расширенными концепциями программирования на Java и создайте сложные приложения.',
-        'technology': 'Java'
-    },
-    {
-        'id': 3,
-        'title': 'Разработка веб-приложений с использованием Django',
-        'description': 'Научитесь создавать мощные веб-приложения, используя Django, популярный фреймворк для Python.',
-        'Продолжительность': '8 недель',
-        'technology': 'Python'
-    },
-    {
-        'id': 4,
-        'title': 'Основы Data Science',
-        'description': 'Изучите основы анализа данных и машинного обучения с использованием Python и библиотеки Pandas.',
-        'technology': 'Python'
-    },
-    {
-        'id': 5,
-        'title': 'JavaScript для начинающих',
-        'description': 'Познакомьтесь с основами языка программирования JavaScript и начните создавать интерактивные веб-страницы.',
-        'technology': 'JavaScript'
-    },
-    {
-        'id': 6,
-        'title': 'Разработка мобильных приложений с использованием React Native',
-        'description': 'Создавайте кросс-платформенные мобильные приложения с использованием React Native и JavaScript.',
-        'technology': 'React Native'
-    },
-    {
-        'id': 7,
-        'title': 'Введение в машинное обучение',
-        'description': 'Погружение в основы машинного обучения с использованием Python и библиотеки Scikit-Learn.',
-        'technology': 'Машинное обучение'
-    }
-]
-    filtered_courses = [course for course in courses_data if course['technology']
-                        in selected_filters] if selected_filters else courses_data
-
+    courses_data = Course.query.all()
+    # filtered_courses = [course for course in courses_data if course.technologies[0]
+    #                     in selected_filters] if selected_filters else courses_data
+    filtered_courses = courses_data
     # Фильтр по поисковому запросу
     if search_query:
         filtered_courses = [course for course in filtered_courses if search_query in course['title'].lower()]
@@ -112,6 +68,5 @@ def courses():
 
 @bp.route('/course/<int:id>')
 def course(id):
-    # Заглушка чтобы ссылки генерились хотя бы 'id'
-    # ...
-    return render_template('course.html')
+    data = Course.query.get(id)
+    return render_template('main/course.html', course=data)
