e78f8a1a59b82eaf96ad03a3efa4a4eeb6fbf3fd
Denis <denis03122002@mail.ru>
2023-11-28 16:13:48 +0700

    Длительность теперь поля

--- app/main/course_filtering.py
+++ app/main/course_filtering.py
@@ -1,10 +1,13 @@
 from flask import request
+from sqlalchemy import Integer, func
 from app.models import Course, School, Technology
 
 
 def filter_courses(filter_dict, selected_filters):
     price_from = request.args.get('price_from')
     price_to = request.args.get('price_to')
+    duration_from = request.args.get('duration_from')
+    duration_to = request.args.get('duration_to')
     search_str = request.args.get('search')
     select_filter_dict = {key: [] for key in filter_dict.keys()}
     for item in selected_filters:
@@ -17,6 +20,12 @@ def filter_courses(filter_dict, selected_filters):
         query = query.filter(Course.price >= float(price_from))
     if price_to:
         query = query.filter(Course.price <= float(price_to))
+    if duration_from:
+        query = query.filter(func.cast(func.replace(func.replace(
+            Course.duration, ' часов', ''), ' часа', ''), Integer) >= int(duration_from))
+    if duration_to:
+        query = query.filter(func.cast(func.replace(func.replace(
+            Course.duration, ' часов', ''), ' часа', ''), Integer) <= int(duration_to))
     if search_str:
         query = query.filter(Course.name.ilike(f'%{search_str}%'))
         # у нас sqlite ?
@@ -24,9 +33,6 @@ def filter_courses(filter_dict, selected_filters):
     if select_filter_dict["Школа"]:
         query = query.join(School).filter(
             School.title.in_(select_filter_dict["Школа"]))
-    if select_filter_dict["Длительность"]:
-        query = query.filter(Course.duration.in_(
-            select_filter_dict["Длительность"]))
     if select_filter_dict["Направления"]:
         query = query.filter(Course.technologies.any(Technology.title.in_(select_filter_dict["Направления"]))
                              )
--- app/main/routes.py
+++ app/main/routes.py
@@ -44,14 +44,9 @@ def courses():
     unique_technologies = Technology.query.with_entities(
         Technology.title).distinct().all()
     unique_schools = School.query.with_entities(School.title).distinct().all()
-    unique_durations = Course.query.with_entities(
-        Course.duration).distinct().all()
-    sorted_durations = sorted(set(
-        duration[0] for duration in unique_durations), key=lambda x: int(x.split()[0]))
     filter_dict = {
         'Направления': [tech[0] for tech in unique_technologies],
         'Школа': [school[0] for school in unique_schools],
-        'Длительность': sorted_durations,
     }
     indexed_filter_dict = enumerate(filter_dict.items())
     filtered_courses = filter_courses(filter_dict, selected_filters)
