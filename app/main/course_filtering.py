b30bbb73b15138bae0fb3251cdf9af7c171e040f
Denis <denis03122002@mail.ru>
2023-11-28 15:04:52 +0700

    Фильтрация

---
+++ app/main/course_filtering.py
@@ -0,0 +1,33 @@
+from flask import request
+from app.models import Course, School, Technology
+
+
+def filter_courses(filter_dict, selected_filters):
+    price_from = request.args.get('price_from')
+    price_to = request.args.get('price_to')
+    search_str = request.args.get('search')
+    select_filter_dict = {key: [] for key in filter_dict.keys()}
+    for item in selected_filters:
+        for key, value in filter_dict.items():
+            if item in value:
+                select_filter_dict[key].append(item)
+                break
+    query = Course.query
+    if price_from:
+        query = query.filter(Course.price >= float(price_from))
+    if price_to:
+        query = query.filter(Course.price <= float(price_to))
+    if search_str:
+        query = query.filter(Course.name.ilike(f'%{search_str}%'))
+        # у нас sqlite ?
+        # query = query.filter(func.similarity(Course.name, search_str) > 0.5)
+    if select_filter_dict["Школа"]:
+        query = query.join(School).filter(
+            School.title.in_(select_filter_dict["Школа"]))
+    if select_filter_dict["Длительность"]:
+        query = query.filter(Course.duration.in_(
+            select_filter_dict["Длительность"]))
+    if select_filter_dict["Направления"]:
+        query = query.filter(Course.technologies.any(Technology.title.in_(select_filter_dict["Направления"]))
+                             )
+    return query.all()
--- app/main/routes.py
+++ app/main/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, request, redirect, url_for
 
 from app import db
 from app.main import bp
+from app.main.course_filtering import filter_courses
 from app.models import User, Course, Technology, School, course_technology
 import datetime
 
@@ -34,39 +35,32 @@ def create():
         db.session.commit()
     return redirect(url_for('main.list'))
 
-@bp.route('/list_courses', methods=['GET', 'POST'])
+
+@bp.route('/list_courses', methods=['GET'])
 def courses():
-    """Уберу все лишнее в след спринте"""
-    if request.method == 'POST':
-        # Обработка формы фильтров
-        selected_filters = request.form.getlist('filter')
-        print(selected_filters)
-    else:
-        # Обработка запроса GET (возможно, уже существующие фильтры)
+    selected_filters = request.form.getlist('filter')
+    if not selected_filters:
         selected_filters = request.args.getlist('filter')
-    #прикрутить фильтрацию по цене
-    price_from = request.form.get('price_from')
-    price_to = request.form.get('price_to')
-    search_query = request.args.get('search', '').lower()
+    unique_technologies = Technology.query.with_entities(
+        Technology.title).distinct().all()
+    unique_schools = School.query.with_entities(School.title).distinct().all()
+    unique_durations = Course.query.with_entities(
+        Course.duration).distinct().all()
+    sorted_durations = sorted(set(
+        duration[0] for duration in unique_durations), key=lambda x: int(x.split()[0]))
     filter_dict = {
-        'Направления': ['Python', 'Golang', 'Data Science', 'Java',
-                 'Kotlin', 'Backend', 'Unity', 'SQL', 'Fronted'],
-        'Школа': ['Stepik', 'Hexlet', 'Geekbrains', 'TOP-academy'],
-        'Длительность': ['3 месяца', '6 месяцев', '9 месяцев', '12 месяцев'],
+        'Направления': [tech[0] for tech in unique_technologies],
+        'Школа': [school[0] for school in unique_schools],
+        'Длительность': sorted_durations,
     }
     indexed_filter_dict = enumerate(filter_dict.items())
-
-    courses_data = Course.query.all()
-    # filtered_courses = [course for course in courses_data if course.technologies[0]
-    #                     in selected_filters] if selected_filters else courses_data
-    filtered_courses = courses_data
-    # Фильтр по поисковому запросу
-    if search_query:
-        filtered_courses = [course for course in filtered_courses if search_query in course['title'].lower()]
-
+    filtered_courses = filter_courses(filter_dict, selected_filters)
     return render_template('main/list_courses.html', courses=filtered_courses, indexed_filter_dict=indexed_filter_dict, select=selected_filters)
+
 
 @bp.route('/course/<int:id>')
 def course(id):
     data = Course.query.get(id)
-    return render_template('main/course.html', course=data)
+    technologies = data.technologies.all()
+    school = School.query.get(data.school_id)
+    return render_template('main/course.html', course=data, technologies=technologies, school=school)
