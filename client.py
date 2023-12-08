1e264663936dc57ccecd88cc5b5387c69c7057d5
Njves <mrpostik@gmail.com>
2023-12-08 17:00:49 +0700

    Внедрил клиент в сервер

--- client.py
+++ client.py
@@ -1,3 +1,18 @@
-import requests
+from time import sleep
 
-requests.post('http://127.0.0.1:5000/parse')
+import requests
+import threading
+import datetime
+
+
+def night_update():
+    is_updated = False
+    while True:
+        print('Нужно обновить' if not is_updated else 'Ждем обновления')
+        if datetime.datetime.now().hour == 16 and not is_updated:
+            is_updated = True
+            requests.post('http://127.0.0.1:5000/parse')
+        if datetime.datetime.now().hour == 17 and is_updated:
+            is_updated = False
+        sleep(1)
+threading.Thread(target=night_update).start()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from app import create_app
-
+import client
 app = create_app()
 if __name__ == '__main__':
     app.config['SESSION_TYPE'] = 'filesystem'
+
     app.run(debug=True)
