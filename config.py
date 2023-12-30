14d0e02df2c588adde586e1a971991b6c11741e1
Njves <mrpostik@gmail.com>
2023-12-30 07:29:59 +0700

    Добавил nginx, обновлил зависимости

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'zxczxc'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     COURSE_PER_PAGE = 26
-    CACHE_TYPE = 'SimpleCache'  # Flask-Caching related configs
-    CACHE_REDIS_HOST = 'localhost'
+    CACHE_TYPE = os.environ.get('CACHE_TYPE')
+    CACHE_REDIS_HOST = os.environ.get('CACHE_HOST')
     CACHE_REDIS_PORT = 6379
     CACHE_DEFAULT_TIMEOUT = 300
